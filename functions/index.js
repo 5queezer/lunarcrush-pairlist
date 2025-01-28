7213f20ab39228196501b8ace46e4e446824d30f
Christian <dev@pojoni.at>
2025-01-28 21:26:03 +0100

    update firebase

--- functions/index.js
+++ functions/index.js
@@ -186,3 +186,6 @@ app.get("/fetchPairs", async (req, res) => {
 });
 
 exports.api = functions.https.onRequest(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
